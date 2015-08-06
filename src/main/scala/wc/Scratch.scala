bce752314f44e5f5b65a72aa46541eb68eabc0c5
d-smith <doug.smith.mail@gmail.com>
2015-08-06 04:09:07 -0600

    Updated sample to roll up sample service call log lines

---
+++ src/main/scala/wc/Scratch.scala
@@ -0,0 +1,8 @@
+package wc
+
+
+object Scratch {
+  def main(args:Array[String]): Unit = {
+    println("uh huh")
+  }
+}
--- src/main/scala/wc/SimpleStreaming.scala
+++ src/main/scala/wc/SimpleStreaming.scala
@@ -4,21 +4,28 @@ import org.apache.spark.streaming.StreamingContext._
 
 object SimpleStreaming {
   def main(args: Array[String]) {
-  val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-  val ssc = new StreamingContext(conf, Seconds(1))
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(10))
 
-  // Create a DStream that will connect to hostname:port, like localhost:9999
-val lines = ssc.socketTextStream("localhost", 9999)
-// Split each line into words
-val words = lines.flatMap(_.split(" "))
+    // Create a DStream that will connect to hostname:port, like localhost:9999
+    val lines = ssc.socketTextStream("localhost", 9999)
 
-val pairs = words.map(word => (word, 1))
-val wordCounts = pairs.reduceByKey(_ + _)
+    // Split each line into tokens.  A line has the app id and the service called
+    val words = lines.flatMap(_.split(" "))
 
-// Print the first ten elements of each RDD generated in this DStream to the console
-wordCounts.print()
+    val servicesByApp = lines.map(l => {
+      val split = l.split(" ")
+      (split(0),split(1))
+    })
 
-ssc.start()             // Start the computation
-ssc.awaitTermination()  // Wait for the computation to terminate
+    val services = servicesByApp map {
+      p => p._2
+    }
+
+    servicesByApp.countByValue().print()
+    services.countByValue().print()
+
+    ssc.start() // Start the computation
+    ssc.awaitTermination() // Wait for the computation to terminate
   }
 }
